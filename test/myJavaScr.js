08421cdd577b1372a91a21f688952c46c6d4c50e
gaojian <gaojian@ftzex.com>
2017-03-01 16:10:00 +0800

    添加注释,验证代码

--- test/myJavaScr.js
+++ test/myJavaScr.js
@@ -1,33 +1,3 @@
-function getWindowSize() {//获取浏览器可见区域高度
-    var client = {
-        y:0
-    };
-    if(typeof document.compatMode != 'undefined' && document.compatMode == 'CSS1Compat') {
-        client.y = document.documentElement.clientHeight;
-    } else if(typeof document.body != 'undefined' && (document.body.scrollLeft || document.body.scrollTop)) {
-        client.y = document.body.clientHeight;
-    }
-    return client;
-}
-function firstLoad(){//首次加载页面的时候
-    var size = getWindowSize();
-    var browseHeight=size.y;
-    var getDiv=document.getElementById('test').style.height;
-    var getDiv=getDiv.replace("px","");
-    var marginDistance=parseInt((browseHeight-getDiv)/2);
-    var marginDistance=marginDistance.toString()+"px";
-    document.getElementById('test').style.marginTop=marginDistance;
-    }
-    function getsize(){//浏览器窗口大小改变的时候
-    var size = getWindowSize();
-    var browseHeight=size.y;
-    var getDiv=document.getElementById('test').style.height;
-    var getDiv=getDiv.replace("px","");
-    var marginDistance=parseInt((browseHeight-getDiv)/2);
-    var marginDistance=marginDistance.toString()+"px";
-    document.getElementById('test').style.marginTop=marginDistance;
-}
-
 
 function middle(){  
     //获取屏幕的高度  
@@ -38,4 +8,5 @@ function middle(){
     d1.style.margin=(heightBig-heightSmall)/2+"px auto";  
 }  
 middle();  
+//监听窗口变化 
 window.onresize=middle; 
